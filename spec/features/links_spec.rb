057d9acce6b3cf45b89b345353bf498486b64095
toby676 <tjcmui@gmail.com>
2016-06-14 11:27:06 +0100

    add link model

--- spec/features/links_spec.rb
+++
@@ -1,4 +0,0 @@
-feature "it has a list of links on the homepage" do
-	scenario "shows a link on the homepage" do
-		visit "/"
-		expect(page).to have_content "http://www.bbc.co.uk"
---
+++ spec/features/links_view_spec.rb
@@ -0,0 +1,10 @@
+feature "it has a list of links on the homepage" do
+	scenario "shows a link on the homepage" do
+    Link.create(url: "http://www.bbc.co.uk", title: "BBC")
+		visit "/"
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('BBC')
+    end
+  end
+end
